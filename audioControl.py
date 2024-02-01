f28ee83d62e5a0b34fc61590d35b8447747c479b
Eli Chen <105672972+EliYuliangChen@users.noreply.github.com>
2024-02-01 12:27:49 -0500

    Update audioControl.py

--- audioControl.py
+++ audioControl.py
@@ -6,8 +6,8 @@ from djitellopy import Tello
 tello = Tello()
 tello.connect()
 
-# model = Model(r"YOURFILELOCATION")
-model = Model(r"YOURFILELOCATION")
+model = Model(r"C:\\Users\\cheny\\Downloads\\vosk-model-small-en-us-0.15")
+# model = Model(r"C:\\Users\\cheny\\Downloads\\vosk-model-small-cn-0.22")
 
 recognizer = KaldiRecognizer(model, 16000)
 
@@ -34,24 +34,30 @@ def getCommand():
 
 def analyzeCommand(command):
     try:
-        if command == "take off":
+        if "take off" in command:
             tello.takeoff()
-        elif command == "land":
+        elif "land" in command:
             tello.land()
-        elif command == "forward":
+        elif "move forwards" in command:
             tello.move_forward(30)
-        elif command == "back":
+        elif "move back" in command:
             tello.move_back(30)
-        elif command == "left":
+        elif "move left" in command:
             tello.move_left(30)
-        elif command == "right":
+        elif "move right" in command:
             tello.move_right(30)
-        elif command == "up":
+        elif "move up" in command:
             tello.move_up(30)
-        elif command == "down":
+        elif "move down" in command:
             tello.move_down(30)
-        elif command == "flip":
+        elif "flip forwards" in command:
             tello.flip_forward()
+        elif "flip back" in command:
+            tello.flip_back()
+        elif "flip left" in command:
+            tello.flip_left()
+        elif "flip right" in command:
+            tello.flip_right()
         else:
             print("I don't understand")
     except Exception:
@@ -60,36 +66,39 @@ def analyzeCommand(command):
 
 def analyzeCNCommand(command):
     try:
-        if command == "起飞":
+        if "起飞" in command:
             tello.takeoff()
-        elif command == "降落":
+        elif "降落" in command:
             tello.land()
-        elif command == "前进":
+        elif "向前飞" in command:
             tello.move_forward(30)
-        elif command == "后退":
+        elif "向后飞" in command:
             tello.move_back(30)
-        elif command == "左":
+        elif "向左飞" in command:
             tello.move_left(30)
-        elif command == "右":
+        elif "向右飞" in command:
             tello.move_right(30)
-        elif command == "上":
+        elif "向上飞" in command:
             tello.move_up(30)
-        elif command == "下":
+        elif "向下飞" in command:
             tello.move_down(30)
-        elif command == "翻":
+        elif "向前翻" in command:
             tello.flip_forward()
+        elif "向后翻" in command:
+            tello.flip_back()
+        elif "向左翻" in command:
+            tello.flip_left()
+        elif "向右翻" in command:
+            tello.flip_right()
         else:
-            print("I don't understand")
+            print("对不起，我不明白你的意思")
     except Exception:
-        print("Something went wrong")
+        print("出现了一些问题")
         pass
 
 while True:
     print("Listening...")
     command = getCommand()
     print("Command: " + command)
-    # English
-    # analyzeCommand(command)
-    # Chinese
-    analyzeCNCommand(command)
+    analyzeCommand(command)
     print(tello.get_battery())
